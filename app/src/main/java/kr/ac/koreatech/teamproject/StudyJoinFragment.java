4f68c2b495d6f1ba175c9cfe67ce890b90db0174
jinwoo <cjw.git@gmail.com>
2021-10-30 22:36:49 +0900

    rest my time... backup

--- app/src/main/java/kr/ac/koreatech/teamproject/StudyJoinFragment.java
+++ app/src/main/java/kr/ac/koreatech/teamproject/StudyJoinFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,6 +14,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.ac.koreatech.teamproject.databinding.FragmentStudyJoinBinding;
 import kr.ac.koreatech.teamproject.databinding.FragmentStudyListBinding;
@@ -63,7 +70,34 @@ public class StudyJoinFragment extends Fragment {
         }
 
         binding = FragmentStudyJoinBinding.inflate(getLayoutInflater());
+        List<String> list = new ArrayList<>();
+        list.add("집으로 가즈앙");
+        list.add("기분 좋은 아침");
+        list.add("안드로이드");
+        list.add("아이폰은 전설이다");
+        list.add("공부가 싫어");
+        list.add("과제도 싫엉");
+        list.add("공부 멈춰!!!!");
+        list.add("안스 어렵다...");
+        list.add("m1맥은 쓰레기야.");
+        list.add("안드로이드 스튜디오 멍청이");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list);
+        binding.framentStudyJoinListView.setAdapter(adapter);
 
+        //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
+        binding.framentStudyJoinListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view, int position, long id) {
+
+                //클릭한 아이템의 문자열을 가져옴
+                String selected_item = (String) adapterView.getItemAtPosition(position);
+
+                //텍스트뷰에 출력
+                System.out.println(selected_item + " 에 가입하려고?");
+            }
+        });
     }
 
     @Override
