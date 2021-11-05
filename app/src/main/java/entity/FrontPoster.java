1a3413585c944d5f6d13ab46b18fd08249c7ac8e
jinwoo <cjw.git@gmail.com>
2021-11-05 10:19:07 +0900

    FrontPoster 주석처리

--- app/src/main/java/entity/FrontPoster.java
+++ app/src/main/java/entity/FrontPoster.java
@@ -2,9 +2,12 @@ package entity;
 
 import android.graphics.Bitmap;
 
+/**
+ * 메인 뷰의 리싸이클러뷰에 표시되는 엔티티 객체
+ */
 public class FrontPoster {
-    private Bitmap bitmap;
-    private String title;
+    private Bitmap bitmap; // 사진
+    private String title; // 제목(교과명)
 
     public FrontPoster(Bitmap bitmap, String title) {
         this.title = title;
