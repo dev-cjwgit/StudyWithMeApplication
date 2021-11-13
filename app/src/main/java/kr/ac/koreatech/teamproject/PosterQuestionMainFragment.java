7e4133bcf1b8d7a906344ea0808131b6d943ac21
jinwoo <cjw.git@gmail.com>
2021-11-13 15:09:01 +0900

    add func

--- app/src/main/java/kr/ac/koreatech/teamproject/PosterQuestionMainFragment.java
+++ app/src/main/java/kr/ac/koreatech/teamproject/PosterQuestionMainFragment.java
@@ -2,11 +2,19 @@ package kr.ac.koreatech.teamproject;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.text.SimpleDateFormat;
+
+import entity.PosterQuestionEntity;
+import kr.ac.koreatech.teamproject.databinding.FragmentPosterQuestionListBinding;
+import kr.ac.koreatech.teamproject.databinding.FragmentPosterQuestionMainBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,36 +22,18 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class PosterQuestionMainFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
+    private FragmentPosterQuestionMainBinding binding;
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private PosterQuestionEntity entity;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public PosterQuestionMainFragment() {
+    public PosterQuestionMainFragment(PosterQuestionEntity entity) {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PosterQuestionMainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PosterQuestionMainFragment newInstance(String param1, String param2) {
-        PosterQuestionMainFragment fragment = new PosterQuestionMainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        this.entity = entity;
     }
 
     @Override
@@ -53,12 +43,25 @@ public class PosterQuestionMainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        binding = FragmentPosterQuestionMainBinding.inflate(getLayoutInflater());
+        binding.questionMainTitleTextView.setText(entity.getTitle());
+        binding.questionMainNameTextView.setText(entity.getName());
+        binding.questionMainBodyTextView.setText(entity.getBody());
+        SimpleDateFormat sDate2 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        binding.questionMainDateTextView.setText(sDate2.format(entity.getDate()));
+        binding.questionMainAnswerTextView.setText(entity.getAnswer().toString());
+
+        binding.questionMainBodyTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_poster_question_main, container, false);
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        actionBar.setTitle("질의응답 상세");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setHasOptionsMenu(true);
+        return binding.getRoot();
     }
 }
