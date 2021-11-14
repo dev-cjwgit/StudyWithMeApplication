1475f724e5e11a42074303b17984430c1c8cd29c
cy080 <cy0804bamboo@naver.com>
2021-11-14 22:51:39 +0900

    1. binding 시도중

--- app/src/main/java/kr/ac/koreatech/teamproject/fragment_bulletin_sharing_materials.java
+++ app/src/main/java/kr/ac/koreatech/teamproject/fragment_bulletin_sharing_materials.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kr.ac.koreatech.teamproject.databinding.FragmentBulletinSharingMaterialsBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link fragment_bulletin_sharing_materials#newInstance} factory method to
@@ -19,6 +21,7 @@ public class fragment_bulletin_sharing_materials extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FragmentBulletinSharingMaterialsBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,6 +56,8 @@ public class fragment_bulletin_sharing_materials extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        binding = FragmentBulletinSharingMaterialsBinding.inflate(getLayoutInflater());
+
     }
 
     @Override
