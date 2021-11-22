3f23a6ce9b01626450b1d28539162b93a1e5dc99
chaney <cy0804bamboo@naver.com>
2021-11-22 19:19:52 +0900

    1. 사용자가 가입한 거 리스트 뷰에 뜨게 수정함

--- app/src/main/java/kr/ac/koreatech/teamproject/PosterListFragment.java
+++ app/src/main/java/kr/ac/koreatech/teamproject/PosterListFragment.java
@@ -49,15 +49,27 @@ public class PosterListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private boolean back_btn = false;
 
     private String mParam1;
     private String mParam2;
+
+    public static Map<String, PosterEntity> list = new HashMap<>();
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     private Object MenuInflater;
 
     private PosterListViewAdapter posterListViewAdapter_2;
 
     public PosterListFragment() {
         // Required empty public constructor
+    }
+
+    public PosterListFragment(boolean back_btn) {
+        this.back_btn = back_btn;
     }
 
     /**
@@ -68,78 +80,7 @@ public class PosterListFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment PosterFragment.
      */
-    public static PosterListFragment newInstance(String param1, String param2) {
-        PosterListFragment fragment = new PosterListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        binding = FragmentPosterListBinding.inflate(getLayoutInflater());
-        posterListViewAdapter = new PosterListViewAdapter();
-        binding.framentPosterListListView.setAdapter(posterListViewAdapter);
-
-        binding.searchLayout.getLayoutParams().height = 0;
-        ArrayList kind = new ArrayList();
-        kind.add("강의");
-        kind.add("자격증");
-        kind.add("기타");
-        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, kind);
-        binding.fragmentPosterListSpiner1.setAdapter(adapter);
-
-/*        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "모바일프로그래밍", "강승우", 38, "안드로이드 스튜디오를 이용하여 앱을 만듭니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "객체지향개발론및실습", "김상진", 42, "객체지향의 5대 원칙 등을 배웁니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "컴퓨터네트워크", "박승철", 45, "컴퓨터의 OSI 7계층에 대해서 "));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "IoT개론및실습", "강승우", 23, "라즈베리파이의 GPIO에 대해서 학습합니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "공학설계", "조태훈", 6, "졸업설계를 위한 아이디어를 구상합니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "스크립트프로그래밍", "한연희", 40, "파이썬의 기초 문법을 배웁니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "일터학습개론", "김영은", 38, "일 또는 일 밖에서 일어나는 학습에 대해서 배웁니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "소프트웨어공학", "김승희", 17, "설계전략, 모델링등에 대해서 배웁니다."));
-        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "자바프로그래밍", "김상진", 32, "자바 기초 문법에 대해 학습합니다."));*/
-
-        //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
-        binding.framentPosterListListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> a_parent, View a_view, int a_position, long a_id) {
-                final PosterEntity item = (PosterEntity) posterListViewAdapter.getItem(a_position);
-                MyFragment.changeFragment(new PosterMainFragment(item.getTitle(), true));
-
-                //텍스트뷰에 출력
-                System.out.println(item.getTitle() + " 에 접속함?");
-            }
-        });
-
-    }
-
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.poster_search_menu, menu);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
-        actionBar.setTitle("참여중인 게시판");
-        actionBar.setDisplayHomeAsUpEnabled(false);
-        setHasOptionsMenu(true);
-        return binding.getRoot();
-    }
-
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private void getLectureList() {
         db.collection("server/data/lectureList/")
@@ -149,11 +90,15 @@ public class PosterListFragment extends Fragment {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             Log.d("TAG", document.getId() + " => " + document.getData());
 //                            posterListViewAdapter_2.append(new PosterEntity(BitmapFactory.decodeResource(getResources() R.drawable.default_image), document.getId(),document.getData().get("profName").toString(),document.getData().get("currPeople"),document.getData().get("introduce").toString(),document.getData().get("category").toString());
-                            posterListViewAdapter_2.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image),
+                            PosterEntity entity1 = new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image),
                                     document.getId(),
                                     document.getData().get("profName").toString(),
                                     Integer.parseInt(document.getData().get("currPeople").toString()),
-                                    document.getData().get("introduce").toString()));
+                                    document.getData().get("introduce").toString());
+
+                            posterListViewAdapter_2.append(entity1);
+                            list.put(document.getId(),entity1);
+
                         }
                     } else {
                         Log.d("TAG", "Error getting documents: ", task.getException());
@@ -184,6 +129,11 @@ public class PosterListFragment extends Fragment {
                 DocumentSnapshot document = task.getResult();
                 if (document.exists()) {
                     Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                    ArrayList<String> temp = (ArrayList<String>) document.get("title");
+                    for (String item : temp) {
+                        PosterEntity temp2 = list.get(item);
+                        posterListViewAdapter.append(temp2);
+                    }
                 } else {
                     Log.d("TAG", "No such document");
                 }
@@ -191,6 +141,78 @@ public class PosterListFragment extends Fragment {
                 Log.d("TAG", "get failed with ", task.getException());
             }
         });
+    }
+
+
+    public static PosterListFragment newInstance(String param1, String param2) {
+        PosterListFragment fragment = new PosterListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        binding = FragmentPosterListBinding.inflate(getLayoutInflater());
+        posterListViewAdapter = new PosterListViewAdapter();
+        binding.framentPosterListListView.setAdapter(posterListViewAdapter);
+        posterListViewAdapter_2 = new PosterListViewAdapter();
+
+        binding.searchLayout.getLayoutParams().height = 0;
+        ArrayList kind = new ArrayList();
+        kind.add("강의");
+        kind.add("자격증");
+        kind.add("기타");
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, kind);
+        binding.fragmentPosterListSpiner1.setAdapter(adapter);
+        getLectureList();
+        getJoinLectureList(firebaseAuth.getCurrentUser().getEmail());
+
+/*        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "모바일프로그래밍", "강승우", 38, "안드로이드 스튜디오를 이용하여 앱을 만듭니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "객체지향개발론및실습", "김상진", 42, "객체지향의 5대 원칙 등을 배웁니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "컴퓨터네트워크", "박승철", 45, "컴퓨터의 OSI 7계층에 대해서 "));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "IoT개론및실습", "강승우", 23, "라즈베리파이의 GPIO에 대해서 학습합니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "공학설계", "조태훈", 6, "졸업설계를 위한 아이디어를 구상합니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "스크립트프로그래밍", "한연희", 40, "파이썬의 기초 문법을 배웁니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "일터학습개론", "김영은", 38, "일 또는 일 밖에서 일어나는 학습에 대해서 배웁니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "소프트웨어공학", "김승희", 17, "설계전략, 모델링등에 대해서 배웁니다."));
+        posterListViewAdapter.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "자바프로그래밍", "김상진", 32, "자바 기초 문법에 대해 학습합니다."));*/
+
+        //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
+        binding.framentPosterListListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a_parent, View a_view, int a_position, long a_id) {
+                final PosterEntity item = (PosterEntity) posterListViewAdapter.getItem(a_position);
+                MyFragment.changeFragment(new PosterMainFragment(item.getTitle(), true));
+
+                //텍스트뷰에 출력
+                System.out.println(item.getTitle() + " 에 접속함?");
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.poster_search_menu, menu);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        actionBar.setTitle("참여중인 강의 게시판");
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        setHasOptionsMenu(true);
+        return binding.getRoot();
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -208,10 +230,8 @@ public class PosterListFragment extends Fragment {
                 actionBar.setTitle("전체 강의 게시판 목록");
                 binding.searchLayout.setLayoutParams(params);
 
-                posterListViewAdapter_2 = new PosterListViewAdapter();
                 binding.framentPosterListListView.setAdapter(posterListViewAdapter_2);
                 getLectureList();
-                getJoinLectureList(firebaseAuth.getCurrentUser().getEmail());
 
 /*                posterListViewAdapter_2.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "과목1", "교수님1", 38, "참여 가능 게시판 테스트1"));
                 posterListViewAdapter_2.append(new PosterEntity(BitmapFactory.decodeResource(getResources(), R.drawable.default_image), "과목2", "교수님2", 38, "참여 가능 게시판 테스트2"));
@@ -231,7 +251,7 @@ public class PosterListFragment extends Fragment {
                         final PosterEntity item = (PosterEntity) posterListViewAdapter_2.getItem(a_position);
 
                         addJoinLecture(firebaseAuth.getCurrentUser().getEmail(), item.getTitle()); // 유저 강의 게시판 가입
-                        Toast.makeText(getActivity(),"강의 게시판에 가입합니다.",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "강의 게시판에 가입합니다.", Toast.LENGTH_SHORT).show();
 
                         System.out.println(item.getTitle() + "에 가입함?");
                     }
@@ -240,7 +260,6 @@ public class PosterListFragment extends Fragment {
             default:
                 break;
         }
-
 
         return super.onOptionsItemSelected(item);
     }
